ce859e4ac2cd160826637b3103da23f440ba75e6
Yoshio Yonezawa <Yoshio@Yoshios-MacBook.local>
2020-12-10 20:09:56 +0900

    modified

--- src/get_tweet.py
+++ src/get_tweet.py
@@ -4,10 +4,11 @@ import pandas as pd
 import settings
 
 
-def gettwitterdata(keyword,dfile):
-
-    # 変数
-    count = 0
+class getTwitterData:
+    """
+    Twitter APIを使って、ツイートデータを取得し、
+    TSVデータに出力する
+    """
 
     # Twitter APIを使用するためのConsumerキー、アクセストークン設定
     CONSUMER_KEY = settings.CONSUMER_KEY
@@ -15,61 +16,67 @@ def gettwitterdata(keyword,dfile):
     ACCESS_TOKEN = settings.ACCESS_TOKEN
     ACCESS_SECRET = settings.ACCESS_SECRET
 
-    # 認証
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    def getTwitterData(keyword, dfile):
 
-    api = tweepy.API(auth, wait_on_rate_limit = True)
+        # 変数
+        count = 0
 
-    # 検索キーワード設定 ※デフォルトでリツイートは除外
-    q = keyword + " -RT"
+        # 認証
+        auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
+        auth.set_access_token(self.ACCESS_TOKEN, self.ACCESS_SECRET)
 
-    # つぶやきを格納するDataFrame
-    column_name = [
-        "user_id",
-        "user_name",
-        "follower",
-        "following",
-        "tweet",
-        "date",
-    ]
-    df_tweets = pd.DataFrame([], columns=column_name)
+        api = tweepy.API(auth, wait_on_rate_limit = True)
 
-    # カーソルを使用してデータ取得
-    try:
-        for tweet in tweepy.Cursor(api.search, q=q, count=15, lang='ja', tweet_mode='extended').items():
+        # 検索キーワード設定 ※デフォルトでリツイートは除外
+        q = keyword + " -RT"
 
-            # つぶやき時間がUTCのため、JSTに変換  ※デバッグ用のコード
-            #jsttime = tweet.created_at + datetime.timedelta(hours=9)
-            #print(jsttime)
+        # つぶやきを格納するDataFrame
+        column_name = [
+            "user_id",
+            "user_name",
+            "follower",
+            "following",
+            "tweet",
+            "date",
+        ]
+        df_tweets = pd.DataFrame([], columns=column_name)
 
-            # つぶやきテキスト(FULL)を取得
-            se_tweet = pd.Series([
-                tweet.user.screen_name,
-                tweet.user.name,
-                tweet.user.followers_count,
-                tweet.user.friends_count,
-                tweet.full_text.replace("\n"," "),
-                tweet.created_at + datetime.timedelta(hours=9)
-            ], index=df_tweets.columns)
-            df_tweets = df_tweets.append(se_tweet, ignore_index=True)
+        # カーソルを使用してデータ取得
+        try:
+            for tweet in tweepy.Cursor(api.search, q=q, count=15, lang='ja', tweet_mode='extended').items():
 
-            count += 1
-            if count % 500 == 0:
-                print("count: {:,}".format(count))
+                # つぶやき時間がUTCのため、JSTに変換  ※デバッグ用のコード
+                #jsttime = tweet.created_at + datetime.timedelta(hours=9)
+                #print(jsttime)
 
-    except tweepy.error.TweepError as e:
-        print (e.reason)
+                # つぶやきテキスト(FULL)を取得
+                se_tweet = pd.Series([
+                    tweet.user.screen_name,
+                    tweet.user.name,
+                    tweet.user.followers_count,
+                    tweet.user.friends_count,
+                    tweet.full_text.replace("\n"," "),
+                    tweet.created_at + datetime.timedelta(hours=9)
+                ], index=df_tweets.columns)
+                df_tweets = df_tweets.append(se_tweet, ignore_index=True)
 
-    # ファイル出力
-    df_tweets.to_csv("../output/" + dfile + ".tsv", sep="\t", encoding="utf-8", index=False)
+                count += 1
+                if count % 500 == 0:
+                    print("count: {:,}".format(count))
+
+        except tweepy.error.TweepError as e:
+            print (e.reason)
+
+        # ファイル出力
+        df_tweets.to_csv("../output/" + dfile + ".tsv", sep="\t", encoding="utf-8", index=False)
 
 
 if __name__ == '__main__':
 
     # 検索キーワードを入力
-    print ('====== Enter Serch KeyWord   =====')
-    keyword = input('>  ')
+    # print ('====== Enter Serch KeyWord   =====')
+    # keyword = input('>  ')
+    keyword = "arxiv"
 
     # 出力ファイル名を入力(相対パス or 絶対パス)
     print ('====== Enter Tweet Data file =====')
---
+++ tests/unit/test_get_tweet.py
@@ -0,0 +1,11 @@
+import pytest
+import src.get_tweet as testObj
+
+
+class TestUnitGetTweet:
+
+    def test_hoge(self):
+        """
+        """
+        # assert hoge == hoge
+        pass
